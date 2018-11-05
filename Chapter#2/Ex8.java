7418a07cf73c3e8f448f0c516b1cd2459a25293b
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-05 14:10:53 +0330

    Exercise#8

---
+++ Chapter#2/Ex8.java
@@ -0,0 +1,17 @@
+package ir.phgnight;
+//Calculates a sample payroll calculation.
+import java.util.Scanner;
+
+public class Ex8 {
+    private String age;
+
+    public static void main(String[] args){
+
+        //Exercise#8:
+        int a, b, c;
+        System.out.prit("Enter an integer:");
+        a = b * c;
+
+    }
+}
+
