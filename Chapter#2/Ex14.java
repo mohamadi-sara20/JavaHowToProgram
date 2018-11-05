f6e92d0f917d38d59c6fedfbcc10de1a65ce0881
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-05 14:28:01 +0330

    Exercise#14

---
+++ Chapter#2/Ex14.java
@@ -0,0 +1,26 @@
+package ir.phgnight;
+//Calculates the product of three numbers.
+import java.util.Scanner;
+
+public class Ex14 {
+    private String age;
+
+    public static void main(String[] args){
+
+        //Exercise#14. a)
+        int a = 1, b = 2, c = 3, d = 4;
+        System.out.println(a + " " + b + " " + c + " " + d);
+
+        //Exercise#14. b)
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        System.out.print(c + " ");
+        System.out.print(d + " ");
+
+        System.out.println();
+        //Exercise#15. c)
+        System.out.printf("%d %d %d %d", a, b, c, d);
+        }
+
+    }
+
