97fce8a5b4df59be3e3a60cf5546963744d4659d
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-11 11:09:42 +0330

    Update

--- Chapter#4/Gas.java
+++ Chapter#4/Gas.java
@@ -1,6 +1,5 @@
 package ir.phgnight;
 
-
 public class Gas {
     public static void main(String[] args) {
 
@@ -9,37 +8,36 @@ public class Gas {
         totalGas = 0;
         totalMiles = 0;
         average = 0;
+        gas = 0;
+
         java.util.Scanner input = new java.util.Scanner(System.in);
 
         System.out.print("Enter miles. Enter -1 to quit. ");
         miles = input.nextInt();
-
-        if (miles != -1){
+        if (miles == -1)
+            System.out.print("Not a valid number!");
+        else {
             System.out.print("Enter gas.");
             gas = input.nextInt();
             totalGas += gas;
             totalMiles += miles;
-            System.out.printf("Miles per gallon for this trip: %.2f%n", (double)miles/(double)gas);
-
+            System.out.printf("Miles per gallon for this trip: %.2f%n", (double) miles / (double) gas);
         }
 
-
-        while (miles != -1){
+        while (miles != -1) {
+            totalGas += gas;
+            totalMiles += miles;
             System.out.print("Enter miles. Enter -1 to quit.");
             miles = input.nextInt();
+            System.out.print("Enter gas.");
+            gas = input.nextInt();
+            System.out.printf("Miles per gallon for this trip: %.2f%n", (double) miles / (double) gas);}
 
-            if (miles != -1){
-                System.out.print("Enter gas. ");
-                gas = input.nextInt();
+        average = (double) totalMiles / (double) totalGas;
 
-                totalGas += gas;
-                totalMiles += miles;
-                System.out.printf("Miles per gallon for this trip: %.2f%n", (double) miles/(double) gas);
-            }}
-        average = (double) totalMiles/(double)totalGas;
-
-        System.out.printf("Overall miles per gallon %.2f%n", average);
+        if (!(Double.isNaN(average)))
+            System.out.printf("Overall miles per gallon %.2f%n", average);
 
 
-        }
     }
+}
