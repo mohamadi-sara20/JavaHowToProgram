f60c5bb250708821edfcf7007146248254968462
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-13 18:10:21 +0330

    #fibonacci

---
+++ Chapter#7/Fibonacci.java
@@ -0,0 +1,22 @@
+package ir.phgnight;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        
+        System.out.print(fibonacci(3));
+    }
+
+
+
+    public static int fibonacci(int n){
+
+        if(n <= 1)
+            return n;
+
+        else
+            return fibonacci(n-1) + fibonacci(n-2);
+
+
+
+    }
+}
