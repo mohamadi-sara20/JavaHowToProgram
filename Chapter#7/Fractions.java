ded0203c6ae7f0c93003fb58862807d6dffb2d81
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-13 11:07:04 +0330

    update

--- Chapter#7/Fractions.java
+++ Chapter#7/Fractions.java
@@ -2,14 +2,15 @@ package ir.phgnight;
 //Question 7.3 of the book
 public class Fractions {
     public static void main(String[] args) {
-        double[] ArraySize = new double[10];
+        final int ArraySize = 10;
+        double[] myArr = new double[ArraySize];
         double total = 0;
-        System.out.printf("Element4: %f%n", ArraySize[3]);
-        ArraySize[9] = 1.667;
-        ArraySize[6] = 3.333;
+        System.out.printf("Element4: %f%n", myArr[3]);
+        myArr[9] = 1.667;
+        myArr[6] = 3.333;
 
-        for(int x = 0; x <ArraySize.length; x++){
-            total += ArraySize[x];
+        for(int x = 0; x <myArr.length; x++){
+            total += myArr[x];
         }
         System.out.printf("Total: %f", total);
     }
