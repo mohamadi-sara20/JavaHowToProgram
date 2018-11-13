ed0b3dde57de8777175d385fc3f2684614b9455c
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-13 13:46:31 +0330

    #AddDisplay

--- Chapter#7/SumColumn.java
+++ Chapter#7/SumColumn.java
@@ -1,17 +1,16 @@
 package ir.phgnight;
 
-import java.util.*;
-import java.util.function.*;
-import java.util.stream.*;
-
 //method sumColumn returns the sum of a specific column by index.
 //It gets an array and a number which is the index of the column whose sum we desire.
+//method display prints the array in a table format. 
 
 
 public class SumColumn {
     public static void main(String[] args) {
-        int[][] myArray = {{67, -56}, {51, 34, 91}};
-        System.out.print(sumColumn(myArray, 1));
+        int[][] myArray = {{67, 26, -56}, {51, 34, 91}};
+        System.out.println(sumColumn(myArray, 1));
+        display(myArray);
+
     }
 
     public static int sumColumn(int[][] array, int n) {
@@ -20,5 +19,29 @@ public class SumColumn {
             sum += array[i][n];
 
         return sum;
+    }
+
+    public static void display(int[][] array) {
+        int m = array[0].length;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length > m)
+                m = array[i].length;
+        }
+
+        for (int i = 0; i < m; i++)
+            System.out.printf("%12s %d", "column#", i + 1);
+
+
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("row#%d", i + 1);
+            for (int j = 0; j < array[i].length; j++)
+                System.out.print("   " + array[i][j] + "         ");
+            System.out.println();
+        }
+
+
     }
 }
