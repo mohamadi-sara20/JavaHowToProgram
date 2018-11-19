1f53a91ad9190ed64694b7ba65569422723b68d4
mohamadi-sara20 <mohamadi.sara20@gmail.com>
2018-11-20 01:00:16 +0330

    Update

--- Chapter7/Prod.java
+++
@@ -1,9 +0,0 @@
-package ir.phgnight;
-
-public class Prod {
-    public static void main(String[] args) {
-        int m = Integer.parseInt(args[0]);
-        int n = Integer.parseInt(args[1]);
-        int p = Integer.parseInt(args[2]);
-        System.out.print(m * n * p);
-}}
