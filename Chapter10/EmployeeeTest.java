775cbfc5c724d94ad9f6e74f4d8d4f2b673c343c
Sara Shahmohamadi <mohamadi.sara20@gmail.com>
2018-11-21 16:13:08 +0330

    Update EmployeeTest.java

--- Chapter10/EmployeeeTest.java
+++ Chapter10/EmployeeeTest.java
@@ -4,11 +4,11 @@
 //Create an array of Employee variables to store references to the various employee objects. In a loop,
 //calculate the payroll for each Employee (polymorphically), and add a $100.00 bonus to the person’s
 //payroll amount if the current month is the one in which the Employee’s birthday occurs.
-
 package JavaHowToProgram.Chapter10;
 
+
 public class EmployeeeTest {
-    public String todayDate = "11-21";
+    public static String todayDate = "11:21";
     public static double earning = 0.0;
 
     public static void main(String[] args) {
@@ -16,40 +16,20 @@ public class EmployeeeTest {
         HourlyEmployeee hourlyEmployeee = new HourlyEmployeee("Saman", "Mo", "469275", 14, 2, 1990, 70, 900);
         CommissionEmployeee commissionEmployeee = new CommissionEmployeee("Milad", "Mo", "23452", 21, 11, 1992, 1000, 0.1);
         BasePlusCommissionEmployeee basePlusCommissionEmployeee = new BasePlusCommissionEmployeee("Ehsan", "Alinejad", "3456", 18, 12, 1991, 900, 0.15, 600);
-
-
-
         Employeee[] employees = new Employeee[4];
-
         employees[0] = salariedEmployeee;
         employees[1] = hourlyEmployeee;
         employees[2] = commissionEmployeee;
         employees[3] = basePlusCommissionEmployeee;
-
         for(Employeee employeee: employees){
-            if((employeee.getBirthday().equals("6:20"))) {
+            if((employeee.getBirthday().equals(todayDate))) {
                 System.out.printf("%s %s gets a bonus because today is his birthday!%n", employeee.getFirstName(), employeee.getLastName());
                 earning = employeee.earnings() + 100;
             }
             else
                 earning = employeee.earnings();
-
             System.out.print(employeee.toString());
             System.out.printf("earned: %f%n%n%n", earning);
-
-
-
         }
-
-
-
-
-
-
-
-
-
     }
-
-
 }
