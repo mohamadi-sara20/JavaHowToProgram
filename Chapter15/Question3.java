46392f87824137efd1b1cacb5ff57051ffe0263a
mohamadi-sara20 <mohamadi.sara20@gmail.com>
2018-12-03 00:25:36 +0330

    Question3Modified

--- Chapter15/Question3.java
+++ Chapter15/Question3.java
@@ -1,9 +1,23 @@
+//Complete the following tasks, assuming that each applies to the same program:
+//a) Write a statement that opens file "oldmast.ser" for input—use ObjectInputStream
+//variable inOldMaster to wrap an InputStream object.
+//b) Write a statement that opens file "trans.ser" for input—use ObjectInputStream vari-
+//able inTransaction to wrap an InputStream object.
+//680
+// Chapter 15 Files, Streams and Object Serialization
+//c) Write a statement that opens file "newmast.ser" for output (and creation)—use
+//ObjectOutputStream variable outNewMaster to wrap an OutputStream.
+//d) Write a statement that reads a record from the file "oldmast.ser". The record is an
+//object of class Account —use ObjectInputStream variable inOldMaster. Assume class
+//Account is the same as the Account class in Fig. 15.9
+//e) Write a statement that reads a record from the file "trans.ser". The record is an object
+//of class TransactionRecord—use ObjectInputStream variable inTransaction.
+//f) Write a statement that outputs a record of type Account to the file "newmast.ser"—use
+//ObjectOutputStream variable outNewMaster.
 package JavaHowToProgram.Chapter15;
 
-import javax.xml.transform.stax.StAXResult;
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Question3 {
